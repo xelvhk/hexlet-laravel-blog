a55ca777bfd62378b77c7c71d0ee2344e8ea13b0
Lex <khlex93@gmail.com>
2024-06-24 00:10:03 +0300

    added app.blade.php layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,8 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
